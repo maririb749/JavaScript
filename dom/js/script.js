6dd444ddcef5c8d9996e118af8dcfb2f331d2ffb
Mariana Santos <maririb51@gmail.com>
2023-04-13 15:35:53 +0100

    DOM, window, document

---
+++ dom/js/script.js
@@ -0,0 +1,21 @@
+/*DOM - Document Object Model
+O DOM (Document Object Model) é a representação de dados dos objetos que compõem a estrutura e o conteúdo de um documento na Web.
+Objeto window
+Representa uma janela aberta no navegador, que contém o objeto DOM.
+
+Cada aba do navegador contém seu próprio objeto window.
+
+Pode ser referenciado por window.
+*/
+
+console.log(window)
+
+/*
+Objeto document
+O objeto document representa o documento HTML aberto no navegador.
+Pode ser referenciado por window.document ou somente document.
+*/
+
+console.log(document);
+
+window.alert("Olá mundo!");
