ad55cd0e058f18a3ef8856f6a4c795324672b18c
Mariana Santos <maririb51@gmail.com>
2023-04-17 10:59:44 +0100

    Dataset

---
+++ dataset/script.js
@@ -0,0 +1,8 @@
+/*Dataset são atriburos adicionais que podemos por no nosso elemento que não
+vão influenciar no comportamento desse elemento na página,porém podem 
+facilitar a comunucão com o javaScript.
+*/
+
+const card1 = document.querySelector('[data-product-id="32"]');
+
+console.log(card1);
